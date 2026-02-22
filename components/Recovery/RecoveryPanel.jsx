67048df2b31f96cbb76f4153c50bf9a235f6a3ae
shravyak-02 <shravyakodangala@gmail.com>
2026-02-22 12:52:06 +0530

    Add RecoveryPanel component for damage control

---
+++ components/Recovery/RecoveryPanel.jsx
@@ -0,0 +1,13 @@
+export default function RecoveryPanel() {
+  return (
+    <div>
+      <h2>Damage Control Mode</h2>
+
+      <ul>
+        <li>Attend all remaining classes</li>
+        <li>Score above 80% in finals</li>
+        <li>Focus on high credit subjects</li>
+      </ul>
+    </div>
+  );
+}

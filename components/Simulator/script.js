6f267433d8946b932121c2986f90943a07600744
Shaarwari <shaarwarib@gmail.com>
2026-02-26 22:00:58 +0530

    Create script.js

---
+++ components/Simulator/script.js
@@ -0,0 +1,41 @@
+function enterUSN() {
+  if (usn.value.trim() !== "") {
+    usnBox.classList.add("hidden");
+    subjectsBox.classList.remove("hidden");
+  }
+}
+
+function openPredict(subject) {
+  subjectsBox.classList.add("hidden");
+  predictBox.classList.remove("hidden");
+  subjectName.innerText = subject;
+  calculate();
+}
+
+function calculate() {
+  let a = attendance.value;
+  let c = cie.value;
+
+  let status = "SAFE";
+  let cls = "safe";
+  let msg = "You are on track.";
+
+  if (a < 70 || c < 20) {
+    status = "AT RISK";
+    cls = "risk";
+    msg = "Immediate action required.";
+  } 
+  else if (a < 75 || c < 30) {
+    status = "DANGER";
+    cls = "danger";
+    msg = "You are close to the cutoff.";
+  }
+
+  output.innerHTML = `
+    <div class="${cls}">
+      <b>${status}</b><br>
+      Attendance: ${a}% | CIE: ${c}<br>
+      ${msg}
+    </div>
+  `;
+}
